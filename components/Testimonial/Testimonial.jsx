1aa790c492edb07c2be8d4b4c0321c185d7b0639
Shaun-Was-Taken <62955605+Shaun-Was-Taken@users.noreply.github.com>
2023-12-25 15:00:46 -0600

    Update Testimonial.jsx

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -9,32 +9,32 @@ export default function Testimonial() {
           <div className={styles.testimonialContainer}>
             <div className={styles.testimonial}>
               <p>
-                "Stopped by kinda late but they were amazing! We got pedicures
+                &quot;Stopped by kinda late but they were amazing! We got pedicures
                 and they removed my old gel nail polish! The girls were so
                 great! Enjoyed talking to them about their home country! Will
-                DEFINITELY be back!!!"
+                DEFINITELY be back!!!&quot;
               </p>
               <p className={styles.author}>- Kendra B. ⭐⭐⭐⭐⭐</p>
             </div>
             <div className={styles.testimonial}>
               <p>
-                "They are new but absolutely good. I was walk-ins and they treat
+                &quot;They are new but absolutely good. I was walk-ins and they treat
                 me right a way with beautiful smile, the salon is clean and
                 hugh. I got my dip nails and they turned out beautiful, I
                 impressed with their attention and how they work gentle on my
                 nails. Will tell my friends about this new nail salon in
-                Lawrence."
+                Lawrence.&quot;
               </p>
               <p className={styles.author}>- Amy L. ⭐⭐⭐⭐⭐</p>
             </div>
             <div className={styles.testimonial}>
               <p>
-                "Love nails has never given me anything but exceptional service!
+                &quot;Love nails has never given me anything but exceptional service!
                 Their techniques are precise and their technicians are truly my
                 favorite nail techs I have ever met. Katie is so sweet and always
                 makes me feel welcome & does an amazing job every time. Lele did
                 my roommates nails perfectly. this is now the only place I will
-                get my nails done in Lawrence KS."
+                get my nails done in Lawrence KS.&quot;
               </p>
               <p className={styles.author}>- Katherine M. ⭐⭐⭐⭐⭐</p>
             </div>
